2cf73e9d6baaf5ad815897041065637dad17ee6e
NekoLuka99 <lukas.schroeter99@gmail.com>
2025-03-31 21:18:37 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,14 +4,14 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-
 
 // Deine Firebase-Konfiguration einfügen:
 const firebaseConfig = {
-  apiKey: "DEIN_API_KEY",
-  authDomain: "DEIN_AUTH_DOMAIN",
-  projectId: "DEIN_PROJECT_ID",
-  storageBucket: "DEIN_STORAGE_BUCKET",
-  messagingSenderId: "DEIN_MESSAGING_ID",
-  appId: "DEINE_APP_ID"
+  apiKey: "AIzaSyChYmFmii4E9Ys31bFc7VOx42Q7ORxoiho",
+  authDomain: "junds-d31db.firebaseapp.com",
+  databaseURL: "https://junds-d31db-default-rtdb.firebaseio.com",
+  projectId: "junds-d31db",
+  storageBucket: "junds-d31db.firebasestorage.app",
+  messagingSenderId: "706326315999",
+  appId: "1:706326315999:web:d8da74bb178b63e2f0fa6a"
 };
-
 // Firebase App & Firestore initialisieren
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
