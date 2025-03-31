21fe7e2a215b8d1d9368e1da13adebfb7dcc2f40
NekoLuka99 <lukas.schroeter99@gmail.com>
2025-03-31 21:50:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
 // main.js
 import { db } from './firebase.js';
 
-// Optional: Konsolentest, ob Firestore verbunden ist
-console.log("Firestore verbunden:", db);
-
-// Weitere globale Logik kannst du hier hinzufügen
-
-// main.js
-import { db } from './firebase.js';
-
 // Benutzername aus localStorage holen
 const username = localStorage.getItem("user");
 
@@ -28,3 +20,6 @@ if (navButtons) {
     });
   }
 }
+
+// Optional: Konsolentest, ob Firestore verbunden ist
+console.log("Firestore verbunden:", db);
