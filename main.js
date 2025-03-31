f0468020de4546c0960a59ab965b4545cc6ee6bb
NekoLuka99 <lukas.schroeter99@gmail.com>
2025-03-31 21:19:05 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+// main.js
+import { db } from './firebase.js';
+
+// Optional: Konsolentest, ob Firestore verbunden ist
+console.log("Firestore verbunden:", db);
+
+// Weitere globale Logik kannst du hier hinzufügen
